229b3c3e60792c2340c6f24f21f75dde33c6f26f
Anthony <oguguaanthony9@gmail.com>
2022-12-08 08:13:23 -0800

    Update

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#!/bin/bash
+include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: A C program that prints with put function
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return(0);
+}
+#*
