09cce8bb9d4170c712a5416b711fac8e61dc78d8
Anthony <oguguaanthony9@gmail.com>
2022-12-08 08:30:39 -0800

    Update

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,12 @@
 #!/bin/bash
 #include <stdio.h>
-/*returns hello world*/
+/**
+ * main-Entry point
+ *
+ * Description:A C program that prints with puts function
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	puts("Programming is like building a multilingual puzzle\n");
