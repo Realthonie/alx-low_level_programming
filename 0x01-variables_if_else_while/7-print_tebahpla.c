dfb6721b8b5d96719bdc14ee5210960866dd5d4b
Anthony <oguguaanthony9@gmail.com>
2022-12-09 13:28:15 -0800

    Update

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	
+	int x = 25;
+	char alphabet[] = "abcdefghijklmnopqrstuvwxyz";
+
+	while (x >= 0)
+	{
+		putchar(alphabet[x]);
+		x--;
+	}
+	putchar('\n');
+	return (0);
+}
