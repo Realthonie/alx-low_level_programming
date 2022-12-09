590499ff6efd1f77e643f225c7d3333d829c5f70
Anthony <oguguaanthony9@gmail.com>
2022-12-09 12:25:09 -0800

    Update

---
+++ 0x01-variables_if_else_while/4-print_alphabet.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main = Entry
+ *
+ * Return: Always 0 (success)
+ */
+int main (void)
+{
+	char alphabet[] = "abcdefghijklmnopqrstuvwxyz";
+	int x = 0;
+
+	while (x <= 25)
+	{
+		if (alphabet[x] == 'q' || alphabet[x] == 'e')
+		{
+			x++;
+			continue;
+		}
+		else
+		{
+			putchar (alphabet[x]);
+			x++;
+		}
+	}
+	putchar('\n');
+	return (0);
+}
