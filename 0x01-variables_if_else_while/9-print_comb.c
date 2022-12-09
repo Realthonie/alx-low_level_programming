1afb769a4b3c1fd2d38b563a905e39a82946d7ff
Anthony <oguguaanthony9@gmail.com>
2022-12-09 14:11:21 -0800

    Update

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - Entry
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int x = 48;
+	/* char alphabet[] = "0123456789";*/
+	while (x < 58)
+	{
+		putchar(x);
+		if (x != 57)
+		{
+			putchat(',');
+			putchar(' ');
+		}
+		x++;
+	}
+	/* putchar('$');*/
+	putchar('\n');
+	return (0);
+}
