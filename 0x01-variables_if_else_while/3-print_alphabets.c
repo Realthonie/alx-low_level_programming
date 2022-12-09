d218c3510b2b94edb53a703a8496dad6d71c7c07
Anthony <oguguaanthony9@gmail.com>
2022-12-09 12:11:48 -0800

    Update

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,1 +1,29 @@
 #include <stdio.h>
+#include <ctype.h>
+/**
+ * main - Entry
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char alphabet[] = "abcdefghijklmnopqrstuvwxyz";
+	int x;
+	int y = 0;
+
+	for (x = 0; x < 2; x++)
+
+	{
+		while (y <= 25)
+		{
+			if (x == 0)
+				putchar(alphabet[y]);
+			else
+				putchar(toupper(alphabet[y]));
+			y++;
+		}
+		y = 0;
+	}
+	putchar('\n');
+	return (0);
+}
