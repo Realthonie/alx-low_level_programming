46bed344c6861b5c6bf40b67130d4c7dd2d5cd11
Anthony <oguguaanthony9@gmail.com>
2022-12-09 13:35:38 -0800

    Update

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
+
 /**
  * main - Entry
- *
  * Return: Always 0 (success)
  */
 int main(void)
@@ -11,6 +11,7 @@ int main(void)
 
 	while (x >= 0)
 	{
+
 		putchar(alphabet[x]);
 		x--;
 	}
