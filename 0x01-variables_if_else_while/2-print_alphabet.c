009b390a181106cf7407b61d301ab43931a29931
Anthony <oguguaanthony9@gmail.com>
2022-12-09 07:18:05 -0800

    Update

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main -Entry
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char alphabet[] = "abcdefghijklmnopqrstuvwxyz";
+	int x = 0;
+
+	while (x <= 25);
+	{
+		putchar(alphabet[x]);
+		x++;
+	}
+	putchar('\n');
+	return (0);
+}
