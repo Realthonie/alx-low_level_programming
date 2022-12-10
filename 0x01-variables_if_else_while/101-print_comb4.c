344892014fa3c5d8d4d54768a38b290647fd5dc5
Anthony <oguguaanthony9@gmail.com>
2022-12-09 19:40:16 -0800

    Update

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+/**
+ * main - Entry
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int x = 48, y, z;
+
+	while (x < 58)
+	{
+		y = x + 1;
+		while (y < 58)
+		{
+			z = y + 1;
+			while (z < 58)
+			{
+				if (y == z || x == z || x == y)
+				{
+					y++;
+					z++;
+					continue;
+				}
+				else
+				{
+					putchar(x);
+					putchar(y);
+					putchar(y);
+					if ((x == 55) && (y == 56) && (z == 57))
+					{
+						putchar('\n');
+						return (0);
+					}
+					z++;
+				}
+			}
+			y++;
+		}
+		x++;
+	}
+	return (0);
+}
