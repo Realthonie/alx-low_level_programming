31561a6e4427ea5982e56815c2c2492d780e867a
Anthony <oguguaanthony9@gmail.com>
2022-12-13 08:25:14 -0800

    Update

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,12 +8,13 @@ void print_alphabet_x10(void)
 {
 	char c;
 	int i;
-	{
 
-		for (i = 1; i <= 10; i++)
-		{	
-		_putchar(c);
+	for (i = 1; i <= 10; i++)
+	{
+		for (c = 'a'; c <= 'z'; c++)
+		{
+			_putchar(c);
 		}
-	_putchar('\n');
+		_putchar('\n');
 	}
 }
