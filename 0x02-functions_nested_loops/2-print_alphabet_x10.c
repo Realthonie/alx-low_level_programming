ccfcb193938282ab9de257785c7f04d77a2a0cd8
Anthony <oguguaanthony9@gmail.com>
2022-12-13 03:05:45 -0800

    Update

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * main - Entry
+ * Description: print ten times alphabets in lowercase
+ * Return: Always 0 (success)
+ */
+void print_alphabet_x10(void)
+{
+	char c;
+	int i;
