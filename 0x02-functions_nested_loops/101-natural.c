23bc5510dad51c3e3a42b3e364182d78bda492dc
Anthony <oguguaanthony9@gmail.com>
2022-12-14 17:10:06 -0800

    Update

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -2,7 +2,7 @@
 
 /**
  *  * main - Get the sum of multiples of 3 or 5 until 1024.
- *   * Return: Always (0).
+ *   * Return: Always 0
  */
 
 int main(void)
@@ -10,7 +10,6 @@ int main(void)
 	int i;
 	int sum = 0;
 	for (i = 0; i < 1024; i++)
-
 	{
 		if ((i % 3) == 0 || (i % 5) == 0)
 			sum += i;
