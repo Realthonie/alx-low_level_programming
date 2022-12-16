5dc6a88958b851e5160ce7c9969532ac8a90ea8b
Anthony <oguguaanthony9@gmail.com>
2022-12-16 01:04:16 -0800

    Update

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -17,7 +17,7 @@ void print_triangle(int size)
 	{
 		for (width = 1; width <= (size - height); width++)
 			_putchar(' ');
-		for (draw = 1; draw <=height; draw++)
+		for (draw = 1; draw <= height; draw++)
 			_putchar('#');
 		_putchar('\n');
 	}
