a4aecf30ad27f295c294e5414cc43169a64f9c0c
Anthony <oguguaanthony9@gmail.com>
2022-12-19 16:53:55 -0800

    Swap

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * swap_int - function to swap two ints
+ * @n: first int
+ * @n: second int
+ *
+ * RETURN: does not return a value
+ */
+
+void swap_int(int *a, int *b)
+{
+	int swap; /* Declaring a local variable 'swap' */
+
+	swap = *b; /* Asssigning the value in the b variable to swap */
+	*b = *a; /* Assigning the value in the a variable to b */
+	*a = swap; /* Assigning the value in swap to a */
+}
