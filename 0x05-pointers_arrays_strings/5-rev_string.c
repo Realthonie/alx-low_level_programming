bdd822cf03f93f9cbaca47ab9f065f647e174c77
Anthony <oguguaanthony9@gmail.com>
2022-12-19 17:32:54 -0800

    Update

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * rev_string - prints string in reverse
+ * @s: the string parameter
+ * Return: returns 0
+ */
+
+void rev_string(char *s)
+{
+	int length;
+	int i;
+	char z;
+
+	length = 0;
+	i = 0;
+	while (s[i++])
+	{length++ }
+
+	for (i = length - 1; i >= length / 2; i--)
+	{
+		z = s[i];
+		s[i] = s[length - i - 1];
+		s[length - i - 1] = z;
+	}
+}
