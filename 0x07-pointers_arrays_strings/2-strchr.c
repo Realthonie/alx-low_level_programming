7f4dbf1e93fe559aff6b79eb77d2e2ba25b6d78d
Anthony <oguguaanthony9@gmail.com>
2023-01-02 21:50:33 +0100

    strchr

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -11,10 +11,10 @@ char *_strchr(char *s, char c)
 {
 	int i = 0;
 
-	while 9s[i] >= '\0')
+	while (s[i] >= '\0')
 	{
-		if 9s[i] == c)
-			return 9&s[i]);
+		if (s[i] == c)
+			return (&s[i]);
 		i++;
 	}
 	return (NULL);
