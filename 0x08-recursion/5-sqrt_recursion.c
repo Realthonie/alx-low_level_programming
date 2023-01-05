0644e7a1bcd8876cda051eb5a357988554ee6e8f
Anthony <oguguaanthony9@gmail.com>
2023-01-05 01:34:28 +0100

    sqrt

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -19,3 +19,25 @@ int is_square_root(int num, int squared)
 	}
 	return (is_square_root(num, squared + 1));
 }
+
+/**
+ * _sqrt_recursion - Entry point
+ * Description - A function that returns the
+ * natural square root of a number
+ * @n: the function accepts an input saved into n
+ * Return: Success (integer)
+ */
+int _sqrt_recursion(int n)
+{
+	int squared = 0;
+	if (n < 0)
+
+	{
+		return (-1);
+	}
+	else if (n == 1)
+	{
+		return (1);
+	}
+	return (is_square_root(n, squared));
+}
