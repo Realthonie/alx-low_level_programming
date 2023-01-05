e63a13791c8e4eaa0fff4b536a054fce693fa09d
Anthony <oguguaanthony9@gmail.com>
2023-01-05 02:09:51 +0100

    palidrome

---
+++ 0x08-recursion/100-is_palidrome.c
@@ -0,0 +1,43 @@
+#include "main.h"
+/**
+ * is_palidrome - Entry point
+ * Description - A function that takes a pointer to an int
+ * *@s: the function accepts an input saved into s
+ * Return: Nothing for now
+ */
+int is_palidrome(char *s)
+{
+	if (*s == 0)
+		return (1);
+	return (check_pal(s, 0, _strlen_recursion(s)));
+}
+
+/**
+ * _strlen_recursion - returns the length of a string
+ * @s: string to calculate the length of
+ *
+ * Return: length of the string
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+		return (0);
+	return (1 + _strlen_recursion(s + 1));
+}
+
+/**
+ * check_pal - checks the characters recursively for palidrome
+ * @s: string to check
+ * @i; iterator
+ * @len: length of the string
+ *
+ * Return: 1 if palidrome, 0 if not
+ */
+int check_pal(char *s, int i, int len)
+{
+	if (*(s + i) != *(s + len - 1))
+		return (0);
+	if (i >= len)
+		return (1);
+	return (check_pal(s, i + 1, len - 1));
+}
