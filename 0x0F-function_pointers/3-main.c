c348302d0644086fc4d1828a6c001e361df5eacc
Anthony <oguguaanthony9@gmail.com>
2023-01-18 19:52:51 +0100

    update

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,43 +1,57 @@
 #include "3-calc.h"
 #include <stdio.h>
-#include <stdlib.h>
+#include <string.h> /*for atoi*/
+#include <stdlib.h> /*for exit*/
 
 /**
- * main - check the code for Holberton School students
- * @argc: argument count
+ * main - Program Entry
+ *
+ * Description: Program that takes 4 arguments where 2 are
+ *              int variables plus an operator to perform
+ *              a function between the two numbers
+ *
+ *              Usage: ./calc num1 operator num2
+ *                         |     |       |     |_ argv[3]
+ *                         |     |       |_ argv[2]
+ *                         |     |_ argv[1]
+ *                         |_ argv[0]
+ *
+ * @argc: argument counter
  * @argv: argument vector
  *
- * Return: Always 0
- */
+ * Return: 0-on success
+*/
 int main(int argc, char *argv[])
 {
-	int a, b;
+	int num1, num2;
+	char *operator;
 
-	int (*operation)(int, int);
-
+	/* Error if arguments are not 4 with the executable */
 	if (argc != 4)
 	{
 		printf("Error\n");
 		exit(98);
 	}
 
-	if (argv[2][1])
+	num1 = atoi(argv[1]); /*if argument is string convert to number*/
+	num2 = atoi(argv[3]); /* as stated above */
+	operator = argv[2];
+
+	/* Error if operator is not null or a 2d array '\*' */
+	if (get_op_func(operator) == NULL || operator[1] != '\0')
 	{
 		printf("Error\n");
 		exit(99);
 	}
 
-	operation = get_op_func(argv[2]);
-
-	if (operation == NULL)
+	/* Error if user tries to divide / (47) or % (37) by 0*/
+	if ((*operator == 47 || *operator == 37) && num2 == 0)
 	{
 		printf("Error\n");
-		exit(99);
+		exit(100);
 	}
 
-	a = atoi(argv[1]);
-	b = atoi(argv[3]);
+	printf("%d\n", get_op_func(operator)(num1, num2));
 
-	printf("%d\n", operation(a, b));
 	return (0);
 }
